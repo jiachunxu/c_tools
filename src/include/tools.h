2f422a143528106427a88d599baf891cafe4ae8b
jsw <954951185@qq.com>
2024-11-14 22:24:25 +0800

    +1

--- src/include/tools.h
+++ src/include/tools.h
@@ -4,10 +4,13 @@
 
 #ifndef C_TOOLS_TOOLS_H
 #define C_TOOLS_TOOLS_H
+
 // 不想敲 shift
 #define and &&
 #define or ||
 #define not !
+typedef char *string;
+//#define string_c char *
 
 // 还是不想敲 shift
 #define is_null(x) ((x) == NULL)
@@ -21,6 +24,5 @@
 // 安全 free , 重复率高
 #define free_ptr(ptr) do { if ((ptr) != NULL) { free((ptr)); (ptr) = NULL; } } while(0)
 
-#define string_c char *
 
 #endif //C_TOOLS_TOOLS_H
