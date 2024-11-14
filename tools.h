34a8af899f09788de3b9c5e05fa53d2294c5e54c
jsw <954951185@qq.com>
2024-11-14 14:51:07 +0800

    +1

---
+++ tools.h
@@ -0,0 +1,21 @@
+//
+// Created by jsw on 24-11-14.
+//
+
+#ifndef C_TOOLS_TOOLS_H
+#define C_TOOLS_TOOLS_H
+// 不想敲 shift
+#define and &&
+#define or ||
+#define not !
+
+// 还是不想敲 shift
+#define is_null(x) ((x) == NULL)
+#define is_not_null(x) ((x) != NULL)
+#define is_zero(x) ((x) == 0)
+#define is_not_zero(x) ((x) != 0)
+#define is_equal(x, y) ((x) == (y))
+#define is_not_equal(x, y) ((x) != (y))
+
+
+#endif //C_TOOLS_TOOLS_H
