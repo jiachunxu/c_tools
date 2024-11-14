1eeeaf946c1da719a6e28d460fc27b75a3c6d0ce
jsw <954951185@qq.com>
2024-11-14 22:32:23 +0800

    +1

--- src/include/tools.h
+++ src/include/tools.h
@@ -9,20 +9,22 @@
 #define and &&
 #define or ||
 #define not !
-typedef char *string;
-//#define string_c char *
 
 // 还是不想敲 shift
 #define is_null(x) ((x) == NULL)
 #define is_not_null(x) ((x) != NULL)
 #define is_zero(x) ((x) == 0)
 #define is_not_zero(x) ((x) != 0)
-//#define is_equal(x, y) ((x) == (y))
-//#define is_not_equal(x, y) ((x) != (y))
+#define is_equal(x, y) ((x) == (y))
+#define is_not_equal(x, y) ((x) != (y))
 
 
-// 安全 free , 重复率高
+// 安全 free , 重复率高 do while 应该是代码结构考虑,不用多余括号
 #define free_ptr(ptr) do { if ((ptr) != NULL) { free((ptr)); (ptr) = NULL; } } while(0)
+
+
+typedef char *String_c;
+//#define string_c char *
 
 
 #endif //C_TOOLS_TOOLS_H
--- test/ main.c
+++ test/ main.c
@@ -4,17 +4,19 @@
 #include "../src/include/tools.h"
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char const *argv[]){
+
+int main(int argc, char const *argv[]) {
     printf("Hello, world!\n");
     int a = 0;
-    if (is_zero(a)) {
+    if (is_zero((a))) {
         printf("a is zero\n");
     } else {
         printf("a is not zero\n");
         return 0;
     }
 
-    string_c str = "what";
+
+    String_c str = "what";
     printf("str is %s\n", str);
 
 }
